0434a1aef64e7dd994c2781550315a0e4e8b31d3
papga9 <gabor.richard.papp@gmail.com>
2025-03-20 11:46:44 +0100

    philosophers

--- main.cpp
+++ main.cpp
@@ -1,26 +1,65 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include "mbed.h"
+#include "rtos.h"
+#include <chrono>
+#include <cstdlib>
 
+Mutex stdio_mutex;
+Mutex forks[5];
 
-// Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
+Thread Thread2(osPriorityNormal, 1024);
+Thread Thread3(osPriorityNormal, 1024);
+Thread Thread4(osPriorityNormal, 1024);
+Thread Thread5(osPriorityNormal, 1024);
 
+void Notify(int num, int state){
+    stdio_mutex.lock();
+    if(state){
+        printf("Philosopher %d is eating \n\r", num);
+    } else{
+        printf("Philosopher %d is thinking \n\r", num);
+    }
+    stdio_mutex.unlock();
+}
+
+void philosopher(const int *num){
+    int philNum = (int)num;
+
+    while (true) {
+        if(forks[philNum - 1].trylock()){
+            if(forks[philNum % 5].trylock()){
+                Notify(philNum, 1);
+                ThisThread::sleep_for(
+                    chrono::milliseconds(rand() % 1000 + 1000)
+                );
+                forks[philNum % 5].unlock();
+                forks[philNum - 1].unlock();
+                Notify(philNum, 0);
+                ThisThread::sleep_for(
+                    chrono::milliseconds(rand() % 2000 + 2000)
+                );
+            }
+            else {
+                forks[philNum - 1].unlock();
+                ThisThread::sleep_for(
+                    chrono::milliseconds(rand() % 100 + 100)
+                );
+            }
+        }
+        else {
+            ThisThread::sleep_for(
+                chrono::milliseconds(rand() % 100 + 100)
+            );
+        }
+    }
+}
 
 int main()
 {
-    // Initialise the digital pin LED1 as an output
-#ifdef LED1
-    DigitalOut led(LED1);
-#else
-    bool led;
-#endif
-
-    while (true) {
-        led = !led;
-        ThisThread::sleep_for(BLINKING_RATE);
-    }
+    Thread2.start(callback(philosopher, (const int *)2));
+    Thread3.start(callback(philosopher, (const int *)2));
+    Thread4.start(callback(philosopher, (const int *)2));
+    Thread5.start(callback(philosopher, (const int *)2));
+    philosopher((const int *)1);
 }
+
+
