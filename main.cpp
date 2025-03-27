c8d81c8d8c6880c6c01184eb8b2a59b691ee27be
papga9 <gabor.richard.papp@gmail.com>
2025-03-27 11:41:08 +0100

    task-1

--- main.cpp
+++ main.cpp
@@ -4,19 +4,63 @@
  */
 
 #include "mbed.h"
+#include "rtos.h"
+#include "MMA8451Q.h"
+#include <cstdio>
 
+//LED PWM outputs
 
-// Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
+PwmOut red(LED_RED);
+PwmOut green(LED_GREEN);
+PwmOut blue(LED_BLUE);
 
-DigitalOut led(LED1);
+//tread
+Thread ThIMU(osPriorityNormal, 2048);
+
+//IMU
+MMA8451Q IMU(PTE25, PTE24, 0x3A);
+
+typedef struct {
+    float acc_x;
+    float acc_y;
+    float acc_z;
+} message_t;
+
+//Queue and Pool object
+Queue<message_t, 1> queue;
+MemoryPool<message_t, 1> mpool;
+
+void GetIMU(){
+    while(true){
+        message_t *message = mpool.alloc();
+        message->acc_x = IMU.getAccX();
+        message->acc_y = IMU.getAccY();
+        message->acc_z = IMU.getAccZ();
+        queue.put(message);
+        ThisThread::sleep_for(50ms);
+    }
+}
+
+void SetLED(){
+    while(true){
+        osEvent evt = queue.get();
+        if (evt.status == osEventMessage){
+            message_t *message = (message_t*)evt.value.p;
+            red.write(message->acc_x);
+            green.write(message->acc_y);
+            blue.write(message->acc_z);
+            printf("x: %f, y: %f, z: %f \n", message->acc_x, message->acc_y, message->acc_z);
+            mpool.free(message);
+        }
+    }
+}
 
 int main()
 {
-    // Initialise the digital pin LED1 as an output
+    red.period_ms(20);
+    green.period_ms(20);
+    blue.period_ms(20);
 
-    while (true) {
-        led = !led;
-        ThisThread::sleep_for(BLINKING_RATE);
-    }
+    ThIMU.start(GetIMU);
+    SetLED();
 }
