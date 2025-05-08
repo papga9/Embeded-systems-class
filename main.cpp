01acb5889cd70e43b655ca6b92e767f66a5d8f9b
papga9 <gabor.richard.papp@gmail.com>
2025-05-08 12:11:16 +0200

    calibrate_test

--- main.cpp
+++ main.cpp
@@ -1,22 +1,54 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include "mbed.h"
+#include "rtos.h"
+#include <cstdio>
+#include <stdint.h>
+#include <stdio.h>
+#include <vector>
+#include "MMA8451Q.h"
 
+#define STOP_FLAG 1
 
-// Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
+MMA8451Q IMU(PTE25, PTE24, 0x3A);
 
-DigitalOut led(LED1);
+Thread thread1(osPriorityNormal, 1024);
+Thread thread2(osPriorityNormal, 1024);
+Thread thread3(osPriorityNormal, 1024);
+
+typedef struct {
+    float accX = 0.0;
+    float accY = 0.0;
+    float accZ = 0.0;
+} mail_t;
+
+Mail<mail_t, 5> mail_box;
+Mail<mail_t, 1> mail_calibrate;
+
+void calibrate() {
+    int NumOfMeasurements = 50;
+
+    float accX = 0.0;
+    float accY = 0.0;
+    float accZ = 0.0;
+
+    for (int i = 0; i < NumOfMeasurements; i++){
+        accX += IMU.getAccX();
+        accY += IMU.getAccY();
+        accZ += IMU.getAccZ();
+    }
+
+    mail_t *mail = mail_calibrate.try_alloc();
+    mail->accX = accX / NumOfMeasurements;
+    mail->accY = accY / NumOfMeasurements;
+    mail->accZ = accZ / NumOfMeasurements;
+    mail_calibrate.put(mail);
+
+    printf("Calibration done");
+}
+
 
 int main()
 {
-    // Initialise the digital pin LED1 as an output
-
-    while (true) {
-        led = !led;
-        ThisThread::sleep_for(BLINKING_RATE);
-    }
+    printf("hallo");
+    thread1.start(calibrate);
+    
 }
