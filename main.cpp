67e8a05747ded1a3fcef71720434105ab959b168
papga9 <gabor.richard.papp@gmail.com>
2025-05-15 12:02:53 +0200

    actally_working_this_time_frfr

--- main.cpp
+++ main.cpp
@@ -14,6 +14,8 @@ Thread thread1(osPriorityNormal, 1024);
 Thread Thread2(osPriorityNormal, 1024);
 Thread thread3(osPriorityNormal, 1024);
 
+Mutex ioMutex;
+
 typedef struct {
     float accX = 0.0;
     float accY = 0.0;
@@ -24,6 +26,58 @@ Mail<mail_t, 5> mail_box;
 Mail<mail_t, 1> mail_calibrate;
 
 Timer dataTimer;
+
+static float x[5] = {0}, y[5] = {0}, z[5] = {0};
+
+void newInput(char axis, float data){
+    switch (axis) {
+    case 'x' :
+        for (int i = 0; i < 4; i++){
+            x[i+1] = x[i];
+        }
+        x[0] = data;
+    break;
+
+    case 'y' :
+        for (int i = 0; i < 4; i++){
+            y[i+1] = y[i];
+        }
+        y[0] = data;
+    break;
+
+    case 'z' :
+        for (int i = 0; i < 4; i++){
+            z[i+1] = z[i];
+        }
+        z[0] = data;
+    break;
+    }
+}
+
+float getAVG(char axis){
+    float result = 0.0;
+
+    switch (axis) {
+    case 'x' :
+        for (int i = 0; i < 5; i++){
+            result += x[i];
+        }
+    break;
+
+    case 'y' :
+        for (int i = 0; i < 5; i++){
+            result += y[i];
+        }
+    break;
+
+    case 'z' :
+        for (int i = 0; i < 5; i++){
+            result += z[i];
+        }
+    break;
+    }
+    return result / 5;
+}
 
 void calibrate() {
     int NumOfMeasurements = 50;
@@ -39,20 +93,29 @@ void calibrate() {
     }
 
     mail_t *mail = mail_calibrate.try_alloc();
-    mail->accX = accX / NumOfMeasurements;
-    mail->accY = accY / NumOfMeasurements;
-    mail->accZ = accZ / NumOfMeasurements;
-    mail_calibrate.put(mail);
+    if (mail != nullptr){
+        mail->accX = accX / NumOfMeasurements;
+        mail->accY = accY / NumOfMeasurements;
+        mail->accZ = accZ / NumOfMeasurements;
+        mail_calibrate.put(mail);
+    }
+    
 
-    printf("Calibration done: %f, %f, %f ", accX, accY, accZ);
+    ioMutex.lock();
+    printf("Kalibrálás elvégezve");
+    ioMutex.unlock();
 }
 
 void readData(float corrX, float corrY, float corrZ){
     mail_t *mail = mail_box.try_alloc();
     if (mail != nullptr){
-        mail->accX = IMU.getAccX() - corrX;
-        mail->accY = IMU.getAccY() - corrY;
-        mail->accZ = IMU.getAccZ() - corrZ;
+        newInput('x', IMU.getAccX());
+        newInput('y', IMU.getAccY());
+        newInput('z', IMU.getAccZ());
+
+        mail->accX = getAVG('x') - corrX;
+        mail->accY = getAVG('y') - corrY;
+        mail->accZ = getAVG('z') - corrZ;
         mail_box.put(mail);
     }
 }
@@ -73,30 +136,36 @@ void calculate(){
 
         float roll = 0.0;
         float pitch = 0.0;
-
-        printf("still alive %f, %f, %f", accX, accY, accZ);
         roll = 180/3.1415 * atan2(accY, accZ);
-        pitch = 180/3.1415 * atan2(-1* accX, sqrt(pow(accY, 2) + pow(accZ, 2)));
+        pitch = (180/3.1415) * atan2(-1* accX, sqrt(pow(accY, 2) + pow(accZ, 2)));
     
-        printf("Roll: %f\n", roll);
-        printf("Pitch: %f\n", pitch);
+        ioMutex.lock();
+        printf("Roll: %f : Pitch: %f\n", roll, pitch);
+        ioMutex.unlock();
     }
     }
 }
 
 void thread2(){
     mail_t *mail = mail_calibrate.try_get();
-    float correctionAccX = mail->accX;
-    float correctionAccY = mail->accY;
-    float correctionAccZ = mail->accZ;
-    mail_calibrate.free(mail);
+
+    float correctionAccX = 0.0;
+    float correctionAccY = 0.0;
+    float correctionAccZ = 0.0;
+
+    if (mail != nullptr){
+        correctionAccX = mail->accX;
+        correctionAccY = mail->accY;
+        correctionAccZ = mail->accZ;
+        mail_calibrate.free(mail);
+    }
 
     while (true){
         if(dataTimer.read() > 0.05){
             readData(correctionAccX, correctionAccY, correctionAccZ);
             dataTimer.reset();
         }else{
-            ThisThread::sleep_for(10ms);
+            ThisThread::sleep_for(50ms);
         }
     }
 }
@@ -104,7 +173,6 @@ void thread2(){
 
 int main()
 {
-    printf("hallo\n");
     thread1.start(calibrate);
     thread1.join();  
 
